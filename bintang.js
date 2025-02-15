82fb6985b1b3102bf4f4ee4704fa32dd4ed3de55
Putra-Developer <iqbalyt87@gmail.com>
2025-02-15 17:26:55 +0700

    Update bintang.js

--- bintang.js
+++ bintang.js
@@ -1,1 +1,12 @@
-
+alert(`ciee ada yang udah makin tua nich`);
+alert(`happy birthday ustajah`);
+alert(`dear adek yang ngeselin tapi imut kan,pasti jadi rindu kalo jauh`);
+alert(`harus jadi porenjes yah untuk tahun ini,harus banyak perubahannya`);
+alert(`jangan tunda' solat lagi  y`)
+alert(`morning person nya di pertahanin juga di hari libur`)
+alert(`tinggalkan yang menyakiti biar ga tersakiti`);
+alert(`malasnya di kurangi`);
+alert(`bang rudinya di tinggal aja`);
+alert(`mudah rezeki y, jangan lupa  traktir`);
+alert(`jangan berhenti bermimpi`);
+alert(`good luck in your life`);
